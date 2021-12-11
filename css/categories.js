110988db92718478121c84adfb7d568fcd647252
YogeshParihar1498 <42130173+YogeshParihar1498@users.noreply.github.com>
2021-12-11 01:48:46 -0500

    first

---
+++ css/categories.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+
+    $('.carousel-showmanymoveone .item').each(function () {
+        var itemToClone = $(this);
+
+        for (var i = 1; i < 6; i++) {
+            itemToClone = itemToClone.next();
+
+            if (!itemToClone.length) {
+                itemToClone = $(this).siblings(':first');
+            }
+
+            itemToClone.children(':first-child').clone()
+                .addClass("cloneditem-" + (i))
+                .appendTo($(this));
+        }
+    });
+
+    $('.carousel-showmanymovetwo .item').each(function () {
+        var itemToClone = $(this);
+
+        for (var i = 1; i < 6; i++) {
+            itemToClone = itemToClone.next();
+
+            if (!itemToClone.length) {
+                itemToClone = $(this).siblings(':first');
+            }
+
+            itemToClone.children(':first-child').clone()
+                .addClass("cloneditem-" + (i))
+                .appendTo($(this));
+        }
+    });
+
+    $('.carousel-showmanymovethree .item').each(function () {
+        var itemToClone = $(this);
+
+        for (var i = 1; i < 6; i++) {
+            itemToClone = itemToClone.next();
+
+            if (!itemToClone.length) {
+                itemToClone = $(this).siblings(':first');
+            }
+
+            itemToClone.children(':first-child').clone()
+                .addClass("cloneditem-" + (i))
+                .appendTo($(this));
+        }
+    });
+
+    $('.carousel-showmanymovefour .item').each(function () {
+        var itemToClone = $(this);
+
+        for (var i = 1; i < 6; i++) {
+            itemToClone = itemToClone.next();
+
+            if (!itemToClone.length) {
+                itemToClone = $(this).siblings(':first');
+            }
+
+            itemToClone.children(':first-child').clone()
+                .addClass("cloneditem-" + (i))
+                .appendTo($(this));
+        }
+    });
+});
